f55971bf896d233d3947db5f91bd90574ef7eb8e
apmechev@ <apmechev@gmail.com>
2015-02-25 11:30:38 +0100

    test.py

---
+++ cr_detection.py
@@ -0,0 +1,276 @@
+#! /usr/bin/env python
+
+''' 
+ABOUT: 
+ This script defines a class used to make ramps with/without cosmic rays for IR detectors,
+ and includes algorithms to detect cosmic rays using methods described in
+ Anderson, R., & Gordon, K. 2011, PASP, 132, 1237
+
+DEPENDS: 
+Python 2.7 
+
+AUTHOR: 
+Rachel E. Anderson, 2012
+
+''' 
+from math import sqrt
+import random
+import numpy as np
+
+class cr_detection_class():
+
+    def __init__(self, readnoise, gain, frame_t, frames_per_group=1):
+
+        self.frame_t = frame_t  #seconds
+        self.gain = gain  #e- per DN
+        self.readnoise = readnoise  #DN per read
+
+
+    def findCRs_2ptdiff(self, counts, rej_thr=3.0): 
+        cr_readNs = []
+        # diff takes 1st-order differences between the elements of counts
+        diff = np.diff(counts)   
+        rn2 = self.readnoise * self.readnoise
+        max_crs = len(diff)   # Upper limit, there won't be this many though...
+    
+        # See if there is a CR in all of these segments
+        for j in xrange(max_crs): 
+            flag = 0
+            cr_free = np.copy(diff)
+        
+            # Remove outliers we already found from cr_free
+            if (len(cr_readNs) != 0):
+                cr_free = np.delete(cr_free,cr_readNs)
+
+            # Calculate ratio to compare to rejection threshold
+            meddiff = np.median(cr_free)
+            pnoise = sqrt(abs(meddiff) * self.gain) / self.gain
+            diff_unc = sqrt(pnoise*pnoise + rn2 + rn2)   # We have two read's            
+            ratio = np.abs(diff - meddiff) / diff_unc
+            sortindx = np.argsort(ratio)[::-1]  # Indices that would reverse-sort an array
+
+            # Remove outliers
+            for i in sortindx:  # Largest first
+                if ((i+1) in cr_readNs): continue   # Skip those already tagged as CRs
+                if ratio[i] > rej_thr:  
+                    cr_readNs.append(i+1)   # This is the frame the CR first appears in
+                    flag = 1
+                break   # If we find one break, if the largest isn't one, break.
+            if (flag == 0): break   # We didn't find any outliers, so we are done
+        return cr_readNs
+
+
+#We assume the CR is in each readN in turn and move the zero point to there,
+# calculate the slope and yintercepts for the two resulting semiramps,
+# and see if the difference of the yintercepts normalized by their errors
+# added in quatrature are within a threshold.
+
+#I call a ramp segment before/after a CR a 'semi-ramp'
+
+    def findCRs_devfit(self, time_in, counts_in, count_errors_in, rej_thr=3.0):
+	indx=self.devfitramp(time_in,counts_in,count_errors_in,rej_thr)
+	readNs=[]
+	if indx:
+		readNs.append(indx)
+		readNs=self.SplitRamp(time_in,counts_in,count_errors_in,0,indx,readNs,rej_thr)
+	return readNs
+
+
+    def SplitRamp(self,time_in,counts_in,cerr_in,begindex,index,readN,rej_thr):
+#	readN.append(index)
+#	import ipdb
+#	ipdb.set_trace()
+	if(len(time_in)<3):
+		return []
+	inlow=self.devfitramp(time_in[:index],counts_in[:index],cerr_in[:index],rej_thr)
+	if inlow:
+		readN.append(inlow+begindex)
+		readnslow=self.SplitRamp(time_in[:index],counts_in[:index],cerr_in[:index],begindex,inlow,readN,rej_thr)
+		
+	inupp=self.devfitramp(time_in[(index+1):],counts_in[(index+1):],cerr_in[(index+1):],rej_thr)
+#	print(begindex,index,inlow,inupp)
+	if inupp:
+		readN.append(inupp+1+index+begindex)
+		readnshigh=self.SplitRamp(time_in[(index+1):],counts_in[(index+1):],cerr_in[(index+1):],index+1,inupp,readN,rej_thr)
+
+	return readN
+
+    def devfitramp(self, time_in, counts_in, count_errors_in, rej_thr=3.0):
+#        import ipdb
+#        ipdb.set_trace()
+	if(len(time_in)<3.):
+		return 0
+        fits,fitserr,fity,fityerr=self.fitline(time_in,counts_in, random_only=1)
+        dev=[(counts_in[i]-(fits*time_in[i]+fity))/(count_errors_in[i]) for i in range(len(time_in))]
+        diffdev=[np.absolute(dev[i]-dev[i-1]) for i in range(1,len(dev))]
+        (mx,ind)=max((v,j) for j,v in enumerate(diffdev))
+	if (mx>rej_thr):
+		return (ind+1)
+	else:
+		return 0
+
+
+    def findCRs_yint(self,time_in,counts_in,count_errors_in,rej_thr=3.0):
+        crcnt = 0
+        goodflags = np.empty(0)
+        cr_readNs = np.array([len(counts_in)-1])  # For the sake of dealing with the last semi-ramp
+                                                  #I add this as a marker (I will remove later)
+        for iteration in xrange(len(counts_in)-1):   # For each possible semi-ramp
+            sortedreadNs = np.sort(cr_readNs)  # I could have added a readN
+                                               #at the end which belongs between.
+            for i in xrange(len(sortedreadNs)):  # I would loop over cr_readNs, but
+                                                 #it is a growing array, instead
+                                                 #I just have to break this once
+                                                 #we loop over all semi-ramps
+                if sortedreadNs[i] in goodflags: continue  # This semi-ramp is fine
+
+                # Pick out CR free ramp segment
+                start = end = None
+                if (crcnt != 0):
+                    if (i == 0):  # If this is the first CR..
+                        start = 0
+                    else:
+                        start = sortedreadNs[i-1]+1 
+                    end = sortedreadNs[i]+1     
+                else:
+                    start = 0
+                    end = len(counts_in)  
+                time = time_in[start:end]
+                counts = counts_in[start:end]
+                count_errors = count_errors_in[start:end]
+
+                # If ramp is too short, don't search for more CR's.
+                # In this case, the ramp was already divided by a CR which would be larger than any found here.
+                if (end - start) < 3:
+                    goodflags = np.append(goodflags,sortedreadNs[i])
+                    continue
+
+                # Now search for CRs
+                n_possible_crs = len(counts)-1
+                slp = np.empty((2,n_possible_crs))
+                slp_err = np.empty((2,n_possible_crs))
+                yint = np.empty((2,n_possible_crs))
+                yint_err = np.empty((2,n_possible_crs))
+                yint_err_sqr = np.empty((2,n_possible_crs))
+                for rn in xrange(len(counts)-1):  # Iterate over possible cr_readNs
+                                                  #(forget first two and last three,
+                                                  #as they must be in line, and remember
+                                                  #that last is not included)
+                    if rn == 0:  # Shift the x-axis to be '0' at single sample
+                        slp[0][rn],slp_err[0][rn],yint[0][rn],yint_err[0][rn] = 0,99999,counts[rn],self.readnoise  
+                        slp[1][rn],slp_err[1][rn],yint[1][rn],yint_err[1][rn] = self.fitline(time[rn+1:]-time[rn],counts[rn+1:],random_only=1)
+                    elif rn == len(counts)-2: # Again, shift the x-axis to be '0 at single sample
+                        slp[0][rn],slp_err[0][rn],yint[0][rn],yint_err[0][rn] = self.fitline(time[:rn+1]-time[rn+1],counts[:rn+1],random_only=1)  
+                        slp[1][rn],slp_err[1][rn],yint[1][rn],yint_err[1][rn] = 0,99999,counts[rn+1],self.readnoise   
+                    else:
+                        slp[0][rn],slp_err[0][rn],yint[0][rn],yint_err[0][rn] = self.fitline(time[:rn+1]-time[rn],counts[:rn+1],random_only=1) 
+                        slp[1][rn],slp_err[1][rn],yint[1][rn],yint_err[1][rn] = self.fitline(time[rn+1:]-time[rn],counts[rn+1:],random_only=1)
+
+                # For each possible CR, calculate the average slope for the Poisson noise (photon noise)
+                slpavg = np.empty(n_possible_crs)
+                for k in xrange(n_possible_crs):
+                    if slp_err[0][k] == 99999:
+                        slpavg[k] = slp[1][k]
+                    elif slp_err[1][k] == 99999:
+                        slpavg[k] = slp[0][k]
+                    else:
+                        slpavg[k] = (slp[0][k]/(slp_err[0][k]*slp_err[0][k]) + slp[1][k]/(slp_err[1][k]*slp_err[1][k]))/(1/(slp_err[0][k]*slp_err[0][k]) + 1/(slp_err[1][k]*slp_err[1][k]))
+                pnoise = np.array([sqrt(abs(slpavg[k])*self.frame_t*self.gain)/self.gain for k in xrange(len(slpavg))])
+
+                #Add photon noise and readnoise in quadrature to get expected uncertainty in the y-intercept
+                yerr_exp = np.array([sqrt(pnoise[k]*pnoise[k] + yint_err[0][k]*yint_err[0][k] + yint_err[1][k]*yint_err[1][k]) for k in xrange(len(pnoise))])   #we have two read's
+            
+                ydiff = np.abs(yint[1] - yint[0])  #random_only=1 doesn't mess with how yints are calculated.
+                ratio = ydiff/yerr_exp
+                rn2add = ratio.argmax()
+                if ratio[rn2add] > rej_thr:
+                    cr_readNs = np.append(cr_readNs,start + rn2add)  
+                    crcnt += 1
+                    break   # Every time a cr is found, go on to next iteration
+                else:
+                    goodflags = np.append(goodflags,sortedreadNs[i])  #the semi-ramp ending at this read number is good to go!
+                    if len(goodflags) == crcnt+1: break  
+
+        #remember, we put the last frame in cr_readNs so we need to remove it
+        to_rm = np.where(cr_readNs == len(counts_in)-1)[0][0]
+        if cr_readNs[to_rm] != len(counts_in)-1:  #it does not equal the last read
+            print 'The last item in list cr_readNs:',cr_readNs[to_rm],' is not the final read: ',len(counts)-1,'.  Do not remove it!'
+            sys.exit()
+        cr_readNs = np.delete(cr_readNs,to_rm)  
+        return cr_readNs + 1  # Jump is detected between frame before CR and frame after CR
+
+
+    # Returns slope, slope error, y-intercept, y-intercept error.
+    # Calculates the fit using a covariance matrix taking into account
+    # correlated and uncorrelated errors.
+    # Note that random_only == 1 if it is yint method and we want only random error
+    # for the y-intercept error
+    def fitline(self, xx, yy, random_only=0):
+        #initial estimate of the slope not considering correlated and uncorrelated errors
+        slope,yint = np.polyfit(xx,yy,1) 
+        photon_noise = sqrt(abs(slope) * self.frame_t * self.gain) / self.gain  
+        nn = len(xx)
+        YY = np.asmatrix(yy).T
+        AA = np.asmatrix([np.ones(nn),xx]).T
+        guts = [np.zeros(nn)] * nn
+        PP = np.asmatrix(guts)
+
+        #photon noise (which is correlated)
+        pn2 = photon_noise * photon_noise   
+        pn0 = pn2   #this is the noise we will count in the first read.  
+        for j in xrange(nn):
+            for i in xrange(j,nn): 
+                PP[i,j] = PP[j,i] = pn2 * (j + 1)   
+                
+        #now add readnoise on the diagonal
+        RR = np.asmatrix(np.identity(nn) * self.readnoise * self.readnoise)  
+        CC = PP + RR
+
+        #calculate slope and y-intercept
+        part1 = AA.T * (CC.I * AA)
+        part2 = AA.T * (CC.I * YY)
+        XX = part1.I * part2
+        bb = XX[0,0]
+        mm = XX[1,0]
+
+        #calculate slope and y-intercept error
+        mm_err = sqrt(part1.I[1,1])
+        if random_only == 1:
+            part1 = AA.T * (RR.I * AA)   #random error only
+        bb_err = sqrt(part1.I[0,0])
+        #chi2 = (YY - AA * XX).T * CC.I * (YY - AA * XX)
+        return mm,mm_err,bb,bb_err
+ 
+
+    # readN is the first read the CR is detected in, so the first read AFTER the CR hit.
+    # Can make a CR-free ramp if you don't include crmag and readN
+    # crmag is in DN.  crmag and readN's should be arrays
+    def mkramp(self, slope, yint, nframes, crmag=None, readN=None):  
+        # Calculate time
+        int_t = (nframes-1)*self.frame_t  
+        time = np.linspace(self.frame_t, int_t + self.frame_t, nframes)
+
+        # Get counts
+        cnts2add = slope*self.frame_t
+        cnts2add_electrons = cnts2add * self.gain
+        counts = np.empty(nframes)   #not actually empty, just has whatever garbage this memory contains.
+        counts[0] = yint + np.random.poisson(lam=cnts2add_electrons) / self.gain  
+        for frame in xrange(1,nframes):
+            #included correlated noise
+            counts[frame] = counts[frame-1] +  np.random.poisson(lam=cnts2add_electrons) / self.gain
+            
+            # Add CR if necessary
+            if crmag and ((frame-1) in readN):   # Occured between frame n and n+1
+                counts[frame] += crmag[np.where(readN == (frame-1))[0][0]] 
+                
+        #now add un-correlated noise
+        counts += [random.normalvariate(0,self.readnoise) for frame in xrange(nframes)]
+
+        #Calculate errors
+        photonNoise = np.array([sqrt(abs(counts[i])*self.gain)/self.gain for i in xrange(nframes)])
+	#photonNoise =np.array([sqrt(abs(counts[i])*0)/self.gain for i in xrange(nframes)])
+        rn2 = self.readnoise*self.readnoise
+        count_errors = np.array([sqrt(photonNoise[i]*photonNoise[i] + rn2) for i in xrange(nframes)])
+        return time, counts, count_errors
+    
+
---
+++ test.py
@@ -0,0 +1,231 @@
+import cr_detection
+import matplotlib.pyplot as plt
+import numpy.random as rnd
+import numpy
+import time as tm
+from scipy.integrate import simps
+import os
+
+frame_t=1
+rdoutnoise=2
+gain=1
+cr_class=cr_detection.cr_detection_class(rdoutnoise,gain,frame_t)
+rnseed=220022
+
+rnd.seed(rnseed)
+
+def makeramp(nframes,csmcmag,rmpslope,cr_class,rnseed):
+	numhits=0
+	falsedets=numpy.zeros((100,20))
+	miss=numpy.zeros((100,20))
+
+
+	readN=numpy.zeros(nframes)
+	cosmagarray=[0]*len(readN)
+
+
+	#add a number of cosmics based on poisson distribution
+	#size of cosmic taken from normal distribution
+	#magnitude csmcmag and SD of 5
+	for j in range(nframes):
+		numhits=rnd.poisson(0.1)
+                if (numhits>0):
+                        readN[j]=j
+                        magadd=numpy.abs(rnd.normal(csmcmag,5))
+                        cosmagarray[j]=magadd*numhits
+			numhits=0
+
+	time,cnts,cnterr = cr_class.mkramp(rmpslope,1,nframes,cosmagarray,readN)
+	
+	return (time,cnts,cnterr,cosmagarray)
+
+
+def detectCR(time,cnts,cnterr,method,threshold,cr_class):
+	if (method=='2pt'):
+		detected=cr_class.findCRs_2ptdiff(cnts,threshold)
+	if (method=='devfit'):
+		detected=cr_class.findCRs_devfit(time,cnts,cnterr,threshold)
+	if (method=='yint'):
+		detected=cr_class.findCRs_yint(time,cnts,cnterr,threshold)	
+
+	return detected
+
+
+def runNramps(numramps,rmpslope,nframes,cosmag=0,method='2pt',thresh=1.0):
+	t_arr=[]
+	c_arr=[]
+	e_arr=[]
+	m_arr=[]
+	d_arr=[]
+	t_arr.append([])
+        c_arr.append([])
+        e_arr.append([])
+        m_arr.append([])
+        d_arr.append([])
+
+	for rampn in range(0,numramps):
+		t,c,e,m=makeramp(nframes,cosmag,rmpslope,cr_class,rnseed)
+		d=detectCR(t,c,e,method,thresh,cr_class)
+		t_arr[0].append(t)
+		c_arr[0].append(c)
+		e_arr[0].append(e)
+		m_arr[0].append(m)
+		d_arr[0].append(d)
+
+	return t_arr[0],c_arr[0],e_arr[0],m_arr[0],d_arr[0]
+
+
+def missfalsedets(time,mags,dets):
+	false=[]
+	miss=[]
+	truedets=[]
+	truemiss=[]
+	cosmics=[]
+
+	for rampn in range(0,len(time)):
+		falseramp=0
+		missramp=0
+		detramp=0
+		Tmissramp=0
+		cosmramp=sum(1 for i in mags[rampn] if i)
+		k=0
+		for val in dets[rampn]:
+			if(mags[rampn][val-1]>0):
+				detramp+=1
+				print('D'),
+			else:
+				print('F'),
+				falseramp+=1
+
+		for val in mags[rampn]:
+			k+=1
+			if(val>0):
+				if (((k) in dets[rampn])):# or (k==len(mags[rampn]))):
+					pass
+				else:
+					print('M'),
+					missramp+=1
+			else:
+				if(not((k+1) in dets[rampn])):
+					Tmissramp+=1
+					#print('X'),
+
+		false.append(falseramp)
+		miss.append(missramp)
+		truedets.append(detramp)
+		truemiss.append(Tmissramp)
+		cosmics.append(cosmramp)
+
+	return false,truedets,miss,truemiss,cosmics
+
+
+def rampsiterate(numramps,method='2pt',iterarray=['thresh',0.1,10,0.1],rmpslope=50,nframes=20,cosmag=20,thresh=3.0):
+        iterable=iterarray[0]
+	if(method=='2pt'):
+                print "iterating over "+iterable+" using 2-point difference method for a sample size of "+str(numramps)+" ramps per point"
+	elif(method=='yint'):
+                print "iterating over "+iterable+" using y-intercept method for a sample size of "+str(numramps)+" ramps per point"
+	elif(method=='devfit'):
+		print "iterating over "+iterable+" using deviation from fit method for a sample size of "+str(numramps)+" ramps per point"
+
+	print " " 
+#	iterable=raw_input("What do you want on x-axis? ['rmpslope','nframes','cosmag','thresh'] ")
+#	itermin=int(raw_input("Min value of x-axis? "))
+#	itermax=int(raw_input("Max value of x-axis? "))
+#	interval=int(raw_input("x interval? "))
+
+#	if(not(iterable=='rmpslope')):
+#		rmpslope=raw_input("What is the ramp slope in DN? ")
+#	if(not(iterable=='nframes')):
+#		nframes=raw_input("What is the number of frames? ")
+#	if(not(iterable=='cosmag')):
+#       	cosmag=raw_input("What is the cosmic magnitude? ")
+#      if(not(iterable=='thresh')):
+#	        thresh=raw_input("What is the detection Threshold in sigma? ")
+
+#	exec(iterable + "= itermin") # sets the iterable variable to first value
+
+	false=numpy.zeros(len(numpy.arange(iterarray[1],iterarray[2],iterarray[3]))+1)
+	truedets=numpy.zeros(len(numpy.arange(iterarray[1],iterarray[2],iterarray[3]))+1)
+	miss=numpy.zeros(len(numpy.arange(iterarray[1],iterarray[2],iterarray[3]))+1)
+	truemiss=numpy.zeros(len(numpy.arange(iterarray[1],iterarray[2],iterarray[3]))+1)
+	dets=numpy.zeros(len(numpy.arange(iterarray[1],iterarray[2],iterarray[3]))+1)
+	
+	i=0
+	for iter1 in numpy.arange(iterarray[1],iterarray[2],iterarray[3]):
+		exec(iterarray[0] + "= iter1")
+		
+		print iterarray[0]		
+                print iter1
+		
+		t1,c1,e1,m1,d1=runNramps(int(numramps),float(rmpslope),int(nframes),float(cosmag),method,float(thresh))
+		f1,td1,M1,tm1,cn1=missfalsedets(t1,m1,d1)
+		
+		false[i]=numpy.sum(f1)
+		truedets[i]=numpy.sum(td1)
+		miss[i]=numpy.sum(M1)
+		truemiss[i]=numpy.sum(tm1)
+		dets[i]=numpy.sum(cn1)
+		i+=1
+	
+
+	return false,truedets,miss,truemiss,dets
+
+
+def makeROC(numramps,method='2pt',iterarray=['thresh',0.1,10,0.1],rmpslope=20,nframes=20,cosmag=50,thresh=3.0 ):
+	f1,td1,M1,tm1,cm1=rampsiterate(numramps,method,iterarray,rmpslope,nframes,cosmag,thresh)
+	truepos=[td1[i]/cm1[i] for i in range(len(cm1)-1)]
+	falsepos=[f1[i]/(nframes*numramps-cm1[i]) for i in range(len(cm1)-1)]
+	x=numpy.arange(0,1.01,0.01)
+	
+	plt.scatter(falsepos,truepos,marker="+",c='k')
+	plt.plot(x,x,linestyle='--',c='r')
+	plt.title("ROC curve for "+method+" detection method varying the "+iterarray[0])
+	plt.ylabel("true positive rate (detections)/(cosmics)")
+	plt.xlabel('false positive rate (false detections)/(total points-cosmics)')
+	plt.grid(b=True,which='major',color='b',linestyle=':')
+	plt.xlim(-0.01,1.01)
+	plt.ylim(-0.01,1.01)
+
+	plt.show()
+	fp1=[falsepos[i] for i in range(len(truepos)-1) if falsepos[i]>0]
+	tr1=[truepos[i] for i in range(len(truepos)-1) if falsepos[i]>0]
+
+	ROCAUC=-simps(tr1,fp1)
+	print "Area Under Curve= "+str(ROCAUC)
+	return ROCAUC, [truepos, falsepos]
+
+
+def plot_falsemiss(numramps,method='2pt',iterarray=['thresh',0.1,10,0.1],rmpslope=20,nframes=20,cosmag=50,thresh=3.0):
+	f1,td1,M1,tm1,cm1=rampsiterate(numramps,method,iterarray,rmpslope,nframes,cosmag,thresh)
+
+	filename="plots_"+str(numramps)+"ramps_"+method+"_rmpslope="+str(rmpslope)+"_nframes="+str(nframes)+"_cosmag="+str(cosmag)+"_thresh="+str(thresh)+"_VAR_"+iterarray[0]+str(iterarray[1])+"->"+str(iterarray[2])
+	
+	falsepos=[f1[i]/(nframes*numramps-cm1[i]) for i in range(len(cm1)-1)]
+	miss=[M1[i]/(cm1[i]) for i in range(len(cm1)-1)]
+
+	fd,=plt.plot(numpy.arange(iterarray[1],iterarray[2],iterarray[3]),falsepos,c='r',label='false detections')
+	ms,=plt.plot(numpy.arange(iterarray[1],iterarray[2],iterarray[3]),miss,c='k',label='missed cosmics')
+
+	plt.legend([fd, ms],["False detections","Missed Cosmic Rays"],loc=4)
+	plt.title("Missed cosmics and False detections for "+method+" method with "+str(numramps)+" runs per point")
+	plt.xlabel(str(iterarray[0])+" varied with a resolution of "+str(iterarray[3]))
+	plt.ylabel("Fraction of missed cosmics or False detections")	
+
+	fig = plt.gcf()
+	fig.set_size_inches(18.5,10.5)
+	fig.savefig('test2png.pdf',dpi=100)
+	plt.show()
+	
+	return 0
+
+
+
+
+
+
+
+
+
+
+
